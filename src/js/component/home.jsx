c7375a7e6d1c8c32f183617aebffad52769b81ff
Macrodose <122926023+Macrodose@users.noreply.github.com>
2024-03-23 13:11:53 +0000

    basic functionality established

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,40 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const Home = () => {
-	return (
-		<div className="d-flex justify-content-center align-items-center vh-100">
-			<div className="lightcontainer">
-				<div className="light red">
+  // Initialize state with default colors
+  const [colors1, setColors1] = useState(true);
+  const [colors2, setColors2] = useState(true);
+  const [colors3, setColors3] = useState(true);
 
-				</div>
-				<div className="light yellow">
-					
-				</div>
-				<div className="light green">
-					
-				</div>
-			</div>
-		</div>
-	);
+  // Function to toggle color and shadow
+  const toggleColor1 = () => {
+    setColors1(!colors1);
+  };
+
+  const toggleColor2 = () => {
+    setColors2(!colors2);
+  };
+
+  const toggleColor3 = () => {
+    setColors3(!colors3);
+  };
+
+
+
+ return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="lightcontainer">
+        <div className={`light ${colors1 ? "red" : "redshadow"}`} onClick={() => toggleColor1()}>
+        </div>
+        <div className={`light ${colors2 ? "yellow" : "yellowshadow"}`} onClick={() => toggleColor2()}>
+        </div>
+        <div className={`light ${colors3 ? "green" : "greenshadow"}`} onClick={() => toggleColor3()}>
+        </div>
+        <button >Cycle!</button>
+      </div>
+      
+    </div>
+  );
 };
 
 export default Home;
