851234a4abeddae34daddbb0ee5fe2381f3ecac3
Macrodose <122926023+Macrodose@users.noreply.github.com>
2024-04-28 19:18:42 +0000

    added deselection

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,37 +1,29 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  // Initialize state with default colors
-  const [colors1, setColors1] = useState(true);
-  const [colors2, setColors2] = useState(true);
-  const [colors3, setColors3] = useState(true);
+ 
+  const [selectedLight, setSelectedLight] = useState(null);
 
-  // Function to toggle color and shadow
-  const toggleColor1 = () => {
-    setColors1(!colors1);
+  const handleClick = (light) => {
+    setSelectedLight(selectedLight === light ? null : light);
   };
 
-  const toggleColor2 = () => {
-    setColors2(!colors2);
-  };
-
-  const toggleColor3 = () => {
-    setColors3(!colors3);
-  };
-
-
-
- return (
+  return (
     <div className="d-flex justify-content-center align-items-center vh-100">
       <div className="lightcontainer">
-        <div className={`light ${colors1 ? "red" : "redshadow"}`} onClick={() => toggleColor1()}>
-        </div>
-        <div className={`light ${colors2 ? "yellow" : "yellowshadow"}`} onClick={() => toggleColor2()}>
-        </div>
-        <div className={`light ${colors3 ? "green" : "greenshadow"}`} onClick={() => toggleColor3()}>
-        </div>
+        <div
+          className={`light ${selectedLight === "red" ? "redshadow" : "red"}`}
+          onClick={() => handleClick("red")}
+        ></div>
+        <div
+          className={`light ${selectedLight === "yellow" ? "yellowshadow" : "yellow"}`}
+          onClick={() => handleClick("yellow")}
+        ></div>
+        <div
+          className={`light ${selectedLight === "green" ? "greenshadow" : "green"}`}
+          onClick={() => handleClick("green")}
+        ></div>
       </div>
-      
     </div>
   );
 };
